dc4d83929802cf188214d568269e801da3ebea0c
Tiago Madeira <tmadeira@gmail.com>
2015-11-27 19:59:09 -0200

    Add D, E from 11-26

--- 11-26/d.cpp
+++ 11-26/d.cpp
@@ -3,13 +3,43 @@
 using namespace std;
 
 #define NMAX 1024
+#define KMAX 131072
 
-char start[NMAX], end[NMAX];
+char str1[NMAX], str2[NMAX];
+unsigned long long dpA[KMAX], dpB[KMAX];
 int k;
 
 int main() {
-  scanf("%s", start);
-  scanf("%s", end);
+  scanf("%s", str1);
+  scanf("%s", str2);
   scanf("%d", &k);
+  int n = strlen(str1);
+  unsigned long long A = 0, B = 0;
+  for (int i = 0; i < n; i++) {
+    string a = &str1[i];
+    char tmp = str1[i];
+    str1[i] = '\0';
+    string b = str1;
+    str1[i] = tmp;
+    int good = (a + b == (string) str2);
+    A+= good;
+    B+= !good;
+  }
+
+  if ((string) str1 == (string) str2) {
+    dpA[0] = 1;
+    dpB[0] = 0;
+  } else {
+    dpA[0] = 0;
+    dpB[0] = 1;
+  }
+
+  for (int n = 1; n <= k; n++) {
+    dpA[n] = (dpA[n-1] * (A-1) + dpB[n-1] * A) % (unsigned long long) (1e9 + 7);
+    dpB[n] = (dpA[n-1] * B + dpB[n-1] * (B-1)) % (unsigned long long) (1e9 + 7);
+  }
+
+  printf("%I64u\n", dpA[k]);
+
   return 0;
 }
--- 11-26/e.cpp
+++ 11-26/e.cpp
@@ -3,65 +3,83 @@
 using namespace std;
 
 int n, m;
-int A[10][10];
 int dx[] = {0, 1, 0, -1};
 int dy[] = {1, 0, -1, 0};
 
-char pd[1<<(5*5)][5*5][5*5];
-// TODO: preencher PD
+char pd[65536][4][4][4][4];
 
-int play(int ax, int ay, int bx, int by);
+int play(int tab, int ax, int ay, int bx, int by);
 
-int play2(int ax, int ay, int bx, int by) {
+inline int M(int x, int y) {
+  return 1<<(4*x+y);
+}
+
+inline int A(int tab, int x, int y) {
+  return tab & M(x, y);
+}
+
+inline int D(int ax, int ay, int bx, int by) {
+  return abs(ax-bx) + abs(ay-by);
+}
+
+int play2(int tab, int ax, int ay, int bx, int by) {
   int out = 0;
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < m; j++) {
-      if (!A[i][j] && (i != ax || j != ay) && (i != bx || j != by)) {
-        A[i][j] = 1;
-        if (!play(ax, ay, bx, by)) {
+      if (!A(tab, i, j) && (i != ax || j != ay) && (i != bx || j != by)) {
+        if (!play(tab | M(i, j), ax, ay, bx, by)) {
           out = 1;
         }
-        A[i][j] = 0;
       }
     }
   }
   return out;
 }
 
-int play(int ax, int ay, int bx, int by) {
+int play(int tab, int ax, int ay, int bx, int by) {
+  if (pd[tab][ax][ay][bx][by] != -1) {
+    return pd[tab][ax][ay][bx][by];
+  }
   if (ax == bx && ay == by) {
     return 1;
   }
   int out = 0;
-  if (!A[ax][ay]) {
+  if (!A(tab, ax, ay)) {
     for (int d = 0; d < 4; d++) {
       int cx = ax + dx[d];
       int cy = ay + dy[d];
       if (0 <= cx && cx < n && 0 <= cy && cy < m) {
-        if (!play2(cx, cy, bx, by)) {
-          out = 1;
+        if (D(cx, cy, bx, by) < D(ax, ay, bx, by)) {
+          if (!play2(tab, cx, cy, bx, by)) {
+            out = 1;
+          }
         }
       }
     }
   }
-  if (!A[bx][by]) {
+  if (!A(tab, bx, by)) {
     for (int d = 0; d < 4; d++) {
       int cx = bx + dx[d];
       int cy = by + dy[d];
       if (0 <= cx && cx < n && 0 <= cy && cy < m) {
-        if (!play2(ax, ay, cx, cy)) {
-          out = 1;
+        if (D(ax, ay, cx, cy) < D(ax, ay, bx, by)) {
+          if (!play2(tab, ax, ay, cx, cy)) {
+            out = 1;
+          }
         }
       }
     }
   }
-  return out;
+  return pd[tab][ax][ay][bx][by] = out;
 }
 
 int main() {
   int ax, ay, bx, by;
-  scanf("%d %d %d %d %d %d", &n, &m, &ax, &ay, &bx, &by);
-  if (abs(bx-ax) + abs(by-ay) >= 8) {
+  scanf("%*d %*d %d %d %d %d", &ax, &ay, &bx, &by);
+  if (max(abs(bx-ax), abs(by-ay)) == 4 && min(abs(bx-ax), abs(by-ay)) <= 2) {
+    printf("First\n");
+    return 0;
+  } else if (max(abs(bx-ax), abs(by-ay)) >= 4) {
     printf("Second\n");
     return 0;
   }
@@ -73,8 +91,7 @@ int main() {
   by -= my;
   n = max(ax, bx) + 1;
   m = max(ay, by) + 1;
-  memset(A, 0, sizeof(A));
-  memset(pd, 0, sizeof(pd));
-  printf("%s\n", play(ax, ay, bx, by) ? "First" : "Second");
+  memset(pd, -1, sizeof(pd));
+  printf("%s\n", play(0, ax, ay, bx, by) ? "First" : "Second");
   return 0;
 }
