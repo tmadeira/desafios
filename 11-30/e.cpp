3ae916a47d8df254e360e11088b001a28d2fdaa5
Tiago Madeira (Rede IME) <madeira@ime.usp.br>
2015-12-01 13:22:38 -0200

    Add E from 11-30

--- 11-30/e.cpp
+++ 11-30/e.cpp
@@ -3,10 +3,14 @@
 using namespace std;
 
 #define NMAX 302
+#define TMAX 100010
+#define MOD ((int) 1e9 + 7)
 
 int A[NMAX];
+int D[NMAX];
 int E[NMAX];
 int M[NMAX];
+int PD[NMAX][TMAX];
 
 int main() {
   int n, q, t;
@@ -20,6 +24,7 @@ int main() {
     scanf("%d %d", &b, &c);
     E[b-1] = c-1;
   }
+  memset(D, 0, sizeof(D));
   for (int s = 0; s < n; s++) {
     int v = s;
     memset(M, 0, sizeof(M));
@@ -27,22 +32,30 @@ int main() {
       M[v] = 1;
 
       t -= A[s];
-      A[E[v]] += A[s];
+      D[E[v]] += A[s];
 
       v = E[v];
-      if (M[v]) {
+      if (M[v] || t < 0) {
         printf("0\n");
         return 0;
       }
     }
   }
-  if (t < 0) {
-    printf("0\n");
-    return 0;
-  }
-  printf("goal = %d\n", t);
   for (int i = 0; i < n; i++) {
-    printf("-> %d\n", A[i]);
+    A[i] += D[i];
   }
+  memset(PD, 0, sizeof(PD));
+  PD[0][0] = 1;
+  for (int i = 1; i <= n; i++) {
+    PD[i][0] = 1;
+    for (int j = 1; j <= t; j++) {
+      PD[i][j] = PD[i-1][j];
+      if (j >= A[i-1]) {
+        PD[i][j] += PD[i][j-A[i-1]];
+        PD[i][j] %= MOD;
+      }
+    }
+  }
+  printf("%d\n", PD[n][t]);
   return 0;
 }
